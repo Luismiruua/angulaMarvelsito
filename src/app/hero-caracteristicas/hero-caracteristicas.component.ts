abe20a21bb680baee1fed190817b35988cb5f8da
Luismiruua <114001610+Luismiruua@users.noreply.github.com>
2023-02-01 22:22:23 +0100

    creacion del servicio

--- src/app/hero-caracteristicas/hero-caracteristicas.component.ts
+++ src/app/hero-caracteristicas/hero-caracteristicas.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../interfaces/hero';
 
 @Component({
   selector: 'app-hero-caracteristicas',
   templateUrl: './hero-caracteristicas.component.html',
   styleUrls: ['./hero-caracteristicas.component.scss']
 })
-export class HeroCaracteristicasComponent {
+export class HeroCaracteristicasComponent implements OnInit{
+  ngOnInit(): void {
+  }
+  constructor() { }
+
+  @Input() hero: Hero | undefined;
 
 }
